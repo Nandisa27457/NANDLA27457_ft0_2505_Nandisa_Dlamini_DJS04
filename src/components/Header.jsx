63e335e3872dd17c78e10ac339d51fbaa65eedd6
Nandisa <shangenandisa@gmail.com>
2025-09-28 11:21:51 +0200

    Added props reausability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="app-header">
       <img  className= "logo" src="./public/podcast.png" alt="Podcast Logo" width="50" height="50"></img>
-      <h1> Podcast App</h1>
+      <h1>{props.children}</h1>
     </header>
   );
 }
