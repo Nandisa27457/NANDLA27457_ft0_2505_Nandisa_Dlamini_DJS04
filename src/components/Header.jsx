82d0a342c79ffcd0268d7032dcb576ddaf6d8d84
Nandisa <shangenandisa@gmail.com>
2025-09-28 11:19:41 +0200

    added a header.jsx file  for the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+      <img  className= "logo" src="./public/podcast.png" alt="Podcast Logo" width="50" height="50"></img>
+      <h1> Podcast App</h1>
+    </header>
+  );
+}
